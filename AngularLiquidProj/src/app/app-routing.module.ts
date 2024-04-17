3973bd6b3ba3c9232aea9d46f5a62604bbe9b5e6
Rahul Kumar <rahulkumar.jsr130299@gmail.com>
2024-04-17 17:52:41 +0530

    Added bootstrap in angular.json, homepage creation and navigation component

--- AngularLiquidProj/src/app/app-routing.module.ts
+++ AngularLiquidProj/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Import HomeComponent
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Define the route for the homepage
+  // Add other routes if needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
