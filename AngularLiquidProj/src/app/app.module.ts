bdc1281bca480b1b37396a554d6dc332676f4462
Chintapalli Pavitra <chintapalli.pavitra@accenture.com>
2024-04-17 19:41:56 +0530

    Login page

--- AngularLiquidProj/src/app/app.module.ts
+++ AngularLiquidProj/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationBarComponent } from './CommonComponents/navigation-bar/navigation-bar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
