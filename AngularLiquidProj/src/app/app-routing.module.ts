bec27b8782e23c24f8342563bcfaabd8ead43676
Prasanna <lakshmi.c.prasanna@accenture.com>
2024-04-25 19:49:25 +0530

    changes for movielist

--- AngularLiquidProj/src/app/app-routing.module.ts
+++ AngularLiquidProj/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // Import HomeComponent
 import { LoginComponent } from './login/login.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch:'full' },
  { path: 'home', component: HomeComponent }, // Define the route for the homepage
  { path: 'login', component: LoginComponent }, // Define the route for the loginpage
- { path: '**', component: LoginComponent }, // Add other routes if needed
+
+ //{ path: '**', component: LoginComponent }, 
+ // Add other routes if needed
+ { path: 'movie-list', component: MovieListComponent }
 ];
 
 @NgModule({
--- AngularLiquidProj/src/app/app.module.ts
+++ AngularLiquidProj/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ParallaxComponent } from './CommonComponents/parallax/parallax.componen
 import { FooterComponent } from './CommonComponents/footer/footer.component';
 import { FeaturedMoviesComponent } from './CommonComponents/featured-movies/featured-movies.component';
 import { QuoteBoxComponent } from './CommonComponents/quote-box/quote-box.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { QuoteBoxComponent } from './CommonComponents/quote-box/quote-box.compon
     FooterComponent,
     FeaturedMoviesComponent,
     QuoteBoxComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
